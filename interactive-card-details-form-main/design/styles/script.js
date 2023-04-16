bd2c8be45209fa95535c633cbd329b82f8c54034
Abdurrahman_X <abolajiabdurrahman@gmail.com>
2023-04-16 11:44:54 +0100

    added comments

--- interactive-card-details-form-main/design/styles/script.js
+++ interactive-card-details-form-main/design/styles/script.js
@@ -3,30 +3,41 @@ const cardHolder = document.querySelector("#card-holder");
 const cardNumber = document.querySelector("#CardNumber");
 const cardMonth = document.querySelector("#month");
 const cardYear = document.querySelector("#year");
+
+const cardName = document.querySelector(".value");
+const cardNum = document.querySelector('.card-number');
 // console.log(cardNumber);
 // console.log(cardMonth);
 // console.log(cardYear);
 
-
+// Add event lister for the input event
 cardHolder.addEventListener("input", function (e) {
-    const cardName = document.querySelector(".value");
+    // Check if the input field is empty
     if (e.target.value === "") {
+        // Update the card name with the original content
         cardName.textContent = "JANE APPLESEED"
     } else {
+        // Update the card name with the real-time input value in uppercase
         cardName.textContent = (e.target.value).toUpperCase();
     }
 })
 
+// Add event lister for the input event
 cardNumber.addEventListener('input', function (e) {
-    const cardNum = document.querySelector('.card-number');
+    // Check if the input field is empty
     if (e.target.value === "") {
+        // Update the card name with the original content
         cardNum.textContent = "0000 0000 0000 0000"
+        // Update the font-size and letter-spacing styles
         cardNum.style.fontSize = '29px';
         cardNum.style.letterSpacing = "1px"
     } else {
+        // Update the card number with the input value in real-time
         cardNum.textContent = e.target.value;
+         // Update the font-size and letter-spacing styles
         cardNum.style.fontSize = '28px';
         cardNum.style.letterSpacing = "2px"
-    }
-    
+    } 
 })
+
+cardMonth
