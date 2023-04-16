036794f158d4506e7a5686ff73a35440451c18b2
Abdurrahman_X <abolajiabdurrahman@gmail.com>
2023-04-16 13:51:31 +0100

    update UI in real-time

--- interactive-card-details-form-main/design/styles/script.js
+++ interactive-card-details-form-main/design/styles/script.js
@@ -3,41 +3,86 @@ const cardHolder = document.querySelector("#card-holder");
 const cardNumber = document.querySelector("#CardNumber");
 const cardMonth = document.querySelector("#month");
 const cardYear = document.querySelector("#year");
+const cvCode = document.querySelector("#cvc")
 
-const cardName = document.querySelector(".value");
-const cardNum = document.querySelector('.card-number');
-// console.log(cardNumber);
-// console.log(cardMonth);
-// console.log(cardYear);
+const cardNameUI = document.querySelector(".value");
+const cardNumUI = document.querySelector('.card-number');
+const expiryDateUI = document.querySelector('.date')
+const cvcUI = document.querySelector('.cvc-value')
 
-// Add event lister for the input event
+
+
+// Add event listener to the card holder's name input
 cardHolder.addEventListener("input", function (e) {
     // Check if the input field is empty
     if (e.target.value === "") {
         // Update the card name with the original content
-        cardName.textContent = "JANE APPLESEED"
+        cardNameUI.textContent = "JANE APPLESEED"
     } else {
         // Update the card name with the real-time input value in uppercase
-        cardName.textContent = (e.target.value).toUpperCase();
+        cardNameUI.textContent = (e.target.value).toUpperCase();
     }
 })
 
-// Add event lister for the input event
+// Add event listener to the card number input
 cardNumber.addEventListener('input', function (e) {
     // Check if the input field is empty
     if (e.target.value === "") {
         // Update the card name with the original content
-        cardNum.textContent = "0000 0000 0000 0000"
+        cardNumUI.textContent = "0000 0000 0000 0000"
         // Update the font-size and letter-spacing styles
-        cardNum.style.fontSize = '29px';
-        cardNum.style.letterSpacing = "1px"
+        cardNumUI.style.fontSize = '29px';
+        cardNumUI.style.letterSpacing = "1px"
     } else {
         // Update the card number with the input value in real-time
-        cardNum.textContent = e.target.value;
+        cardNumUI.textContent = e.target.value;
          // Update the font-size and letter-spacing styles
-        cardNum.style.fontSize = '28px';
-        cardNum.style.letterSpacing = "2px"
+        cardNumUI.style.fontSize = '28px';
+        cardNumUI.style.letterSpacing = "2px"
     } 
 })
 
-cardMonth
+// Add event listener to the expiry month and year
+cardMonth.addEventListener('input', updateDateInput);
+cardYear.addEventListener('input', updateDateInput);
+
+// Function to update the expiry month and year values.
+function updateDateInput(e) {
+    // Get the values of the month and year input fields
+    var month = cardMonth.value;
+    var year = cardYear.value;
+    
+    // Check if both input fields are empty
+    if (month === "" && year === "") {
+        // Update the expiry dates with the original content
+        expiryDateUI.textContent = "00/00";
+
+        // Check if the month input field is empty
+    } else if (month === "") {
+        // Update the UI accordingly
+        expiryDateUI.textContent = `00/${year}`;
+
+        // Check if the year input field is empty
+    } else if (year === "") {
+        // Update the UI accordingly
+        expiryDateUI.textContent = `${month}/00`
+
+    } else {
+        // Update the expiry dates in real-time
+        expiryDateUI.textContent = `${month}/${year}`
+    }
+}
+
+// Add event listener to cvc-code
+cvCode.addEventListener('input', function (e) {
+    // Check if input field is empty
+    if (e.target.value === "") {
+        // Update the cvc code with the original content
+        cvcUI.textContent = "000"
+    } else {
+        // Update the cvc code with the input value in real-time
+        cvcUI.textContent = e.target.value;
+    } 
+})
+
+//---------------------------- FORM VALIDATION ---------------------------------
